28b8d225198b8e25d8111a8e6b66d65f9a6afcd8
Abayev <240918@astanait.edu.kz>
2025-07-04 18:05:33 +0500

    array implementation

--- src/main/java/com/example/homework_1/arrayImplementation/ArrayOperations.java
+++ src/main/java/com/example/homework_1/arrayImplementation/ArrayOperations.java
@@ -2,12 +2,39 @@ package com.example.homework_1.arrayImplementation;
 
 public class ArrayOperations {
 
+    /**
+    Time complexity: O(n)
+     Space complexity: O(k) k is the position
+     shifting elements with using method System.arraycopy
+     */
     public static void shiftLeftSystemCopy(int[] array, int positions) {
+        if(array == null || positions < 0 || positions >= array.length) return;
+
+
+        System.arraycopy(array,positions,array,0,array.length - positions);
+
+        for(int i = array.length - positions; i < array.length; i++) {
+           array[i] = 0;
+        }
     }
 
-
-    /** Shift array elements using manual for loop */
+    /** Shift array elements using manual for loop
+     * Time complexity:O(n)
+     * Space complexity: O(k) where is the k is position
+     * */
     public static void shiftLeftManualLoop(int[] array, int positions) {
+        if(array == null || positions < 0 || positions >= array.length) return;
+        if(positions == 0) return;
+
+        for(int i = 0; i < array.length - positions; i++) {
+            if(i + positions >= array.length){
+                System.out.println("Out of bounds " + i + " shift " + positions);
+            }
+            array[i] = array[i + positions];
+        }
+        for(int i = array.length - positions; i < array.length; i++) {
+            array[i] = 0;
+        }
     }
 
 }
---
+++ src/main/java/com/example/homework_1/arrayImplementation/ArrayTest.java
@@ -0,0 +1,73 @@
+package com.example.homework_1.arrayImplementation;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Random;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        StringBuilder rep =  new StringBuilder();
+        rep.append("Performance Test Array").append("\n");
+
+        rep.append(runTest("Array shift Performance"));
+
+        //wtire to file
+        try(PrintWriter writer = new PrintWriter(new File("Performance_Test_Array.txt"))) {
+            writer.write(rep.toString());
+        } catch (IOException e) {
+            System.out.println("Error to write to file" + e.getMessage());
+        }
+    }
+
+    public static String runTest(String name){
+        StringBuilder res = new StringBuilder();
+        res.append("Testing ").append(name).append("\n");
+
+        int[] sizes = {1000,10_000,100_000,1_000_000};
+        int[] shifts = {1,10,100,1000};
+
+        for(int size: sizes){
+            res.append("Size ").append(size).append("\n");
+
+            for(int shift: shifts){
+                if(shift >= size)continue;
+
+            int[] baseArr = generateArray(size);
+            int[] arrForSystem = Arrays.copyOf(baseArr, baseArr.length);
+            int[] arrForManual = Arrays.copyOf(baseArr, baseArr.length);
+
+            //  System.arraycopy
+                long startTimeSystem = System.nanoTime();
+                ArrayOperations.shiftLeftSystemCopy(arrForSystem,shift);
+                long endTimeSystem = System.nanoTime();
+
+            // Manual loop shifts
+                long startTimeManual = System.nanoTime();
+                ArrayOperations.shiftLeftManualLoop(arrForManual,shift);
+                long endTimeManual = System.nanoTime();
+
+                long elapsedTimeSystem = endTimeSystem - startTimeSystem;
+                long elapsedTimeManual = endTimeManual - startTimeManual;
+
+                res.append(String.format("Shift: %-4d | System.arraycopy: %-8d ns | Manual Loop: %-8d ns\n",
+                        shift, elapsedTimeSystem, elapsedTimeManual));
+            }
+            res.append("\n");
+        }
+
+        return res.toString();
+    }
+
+    private static int[]  generateArray(int size){
+        Random rand = new Random();
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            array[i] = rand.nextInt(100);
+        }
+
+        return array;
+    }
+}
